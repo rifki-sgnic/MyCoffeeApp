7ad64bc8dd8c96c23d8d0f58a43c61494021b120
rifki-sgnic <mrifki028@gmail.com>
2025-10-02 22:36:43 +0700

    ``` refactor: Remove unused Toast import
    Removes an unnecessary import of the `Toast` class from `MainActivity.kt`.

--- app/src/main/java/com/example/mycoffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycoffeeapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
