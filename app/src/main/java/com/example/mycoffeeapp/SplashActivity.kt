94834a06d5c78d761fdd2a16b41781c857e05126
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2025-10-04 02:40:30 +0700

    Merge pull request #4
    * feat: Implement native splash screen and update app icon

--- app/src/main/java/com/example/mycoffeeapp/SplashActivity.kt
+++ app/src/main/java/com/example/mycoffeeapp/SplashActivity.kt
@@ -1,51 +1,30 @@
 package com.example.mycoffeeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val splashScreen = installSplashScreen()
-
-            splashScreen.setKeepOnScreenCondition { true }
-
-            splashScreen.setOnExitAnimationListener { provider ->
-                provider.view.animate()
-                    .alpha(0f)
-                    .setDuration(300L)
-                    .withEndAction { provider.remove() }
-                    .start()
-            }
+            installSplashScreen()
         }
 
         super.onCreate(savedInstanceState)
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
             setContentView(R.layout.activity_splash)
-
-            enableEdgeToEdge()
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-
-            // Add a short delay so the splash is visible
-            Handler(Looper.getMainLooper()).postDelayed({
-                goToMain()
-            }, 1000)
-        } else {
-            goToMain()
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMain()
+        }, 2000)
     }
 
     private fun goToMain() {
